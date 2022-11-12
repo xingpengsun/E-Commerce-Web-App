5059ae1d5d7bea71ff7f0fa9230d992427032a6e
xingpengsun <>
2022-11-12 16:15:37 -0600

    add feat: Admin

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,6 +1,9 @@
 package com.shopme.admin.user;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +13,7 @@ import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Service
+@Transactional
 public class UserService {
 	
 	@Autowired
@@ -30,7 +34,21 @@ public class UserService {
 	}
 
 	public void save(User user) {
-		encodePassword(user);
+		boolean isUpdatingUser = (user.getId() !=null);
+		
+		if (isUpdatingUser) {
+			User existingUser = userRepo.findById(user.getId()).get();
+			
+			if (user.getPassword().isEmpty()) {
+				user.setPassword(existingUser.getPassword());
+			} else {
+				encodePassword(user);
+			}
+			
+		} else {
+			encodePassword(user);
+		}
+		
 		userRepo.save(user);	
 	}
 	
@@ -39,9 +57,42 @@ public class UserService {
 		user.setPassword(encodedPassword);
 	}
 	
-	public boolean isEmailUnique(String email) {
+	public boolean isEmailUnique(Integer id, String email) {
 		User userByEmail = userRepo.getUserByEmail(email);
 		
-		return userByEmail == null;
+		if (userByEmail == null) return true;
+		
+		boolean isCreatingNew = (id==null);
+		
+		if (isCreatingNew) {
+			if(userByEmail !=null) return false;
+		} else {
+			if (userByEmail.getId()!=id) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	public User get(Integer id) throws UserNotFoundException {
+		try {
+		return userRepo.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			throw new UserNotFoundException("Could not find any user with ID "+id);
+		}
+	}
+	
+	public void delete(Integer id) throws UserNotFoundException {
+		Long countById = userRepo.countById(id);
+		if (countById ==null || countById ==0) {
+			throw new UserNotFoundException("Could not find any user with ID "+id);
+		}
+		
+		userRepo.deleteById(id);
+	}
+	
+	public void updateUserEnabledStatus(Integer id, boolean enabled) {
+		userRepo.updateEnabledStatus(id, enabled);
 	}
 }
--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -96,4 +96,26 @@ public class UserRepositoryTests {
 		assertThat(user).isNotNull();
 	}
 	
+	@Test
+	public void testCountById() {
+		Integer id = 1;
+		Long countById = repo.countById(id);
+		
+		assertThat(countById).isNotNull().isGreaterThan(0);
+	}
+	
+	@Test
+	public void testDisableUser() {
+		Integer id=1;
+		repo.updateEnabledStatus(id, false);
+		
+	}
+	
+	@Test
+	public void testEnableUser() {
+		Integer id=3;
+		repo.updateEnabledStatus(id, true);
+		
+	}
+	
 }
